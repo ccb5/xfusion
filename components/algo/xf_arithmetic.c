bb9aacad35f8c5c6c157f538005e5d856e8d07fe
ccb5 <b94e@outlook.com>
2025-05-28 16:24:25 +0800

    🐞 fix(algo-am): 移除无效操作

--- components/algo/xf_arithmetic.c
+++ components/algo/xf_arithmetic.c
@@ -36,7 +36,7 @@ uint32_t xf_am_log2_u32(uint32_t n)
     if (n & 0x0000ff00) { result +=  8; n >>=  8; }
     if (n & 0x000000f0) { result +=  4; n >>=  4; }
     if (n & 0x0000000c) { result +=  2; n >>=  2; }
-    if (n & 0x00000002) { result +=  1; n >>=  1; }
+    if (n & 0x00000002) { result +=  1; /* n >>=  1; */ }
 /* *INDENT-ON* */ 
     return result;
 }
