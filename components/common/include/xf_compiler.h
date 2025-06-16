c99f5cab3b2a8b1019a406280fdba68cfb75edb6
ccb5 <b94e@outlook.com>
2025-06-16 15:59:39 +0800

    🦄 refactor(common): 修改格式

--- components/common/include/xf_compiler.h
+++ components/common/include/xf_compiler.h
@@ -40,35 +40,35 @@ extern "C" {
  * 被 __weak 修饰的符号（变量或函数）
  * 可以被 **没有** 用 __weak 修饰的符号覆盖或重定义.
  */
-#define __weak                          __attribute__((weak))
+#   define __weak                       __attribute__((weak))
 #endif
 
 #if !defined(__used)
 /**
  * @brief 编辑器不会优化掉被 __used 修饰的符号, 即使在代码中即使没有使用到该符号.
  */
-#define __used                          __attribute__((__used__))
+#   define __used                       __attribute__((__used__))
 #endif
 
 #if !defined(__const)
 /**
  * @brief 告诉编译器该值是常量.
  */
-#define __const                         __attribute__((__const__))
+#   define __const                      __attribute__((__const__))
 #endif
 
 #if !defined(__deprecated)
 /**
  * @brief 标注已弃用的函数.
  */
-#define __deprecated                    __attribute__((__deprecated__))
+#   define __deprecated                 __attribute__((__deprecated__))
 #endif
 
 #if !defined(__aligned)
 /**
  * @brief 指定变量或结构体的对齐方式（单位字节）.
  */
-#define __aligned(x)                    __attribute__((aligned(x)))
+#   define __aligned(x)                 __attribute__((aligned(x)))
 #endif
 
 #if !defined(__packed)
@@ -76,7 +76,7 @@ extern "C" {
  * @brief 取消结构在编译过程中的优化对齐,
  *        按照实际占用字节数进行对齐.
  */
-#define __packed                        __attribute__((packed))
+#   define __packed                     __attribute__((packed))
 #endif
 
 #if !defined(struct_packed)
@@ -95,21 +95,21 @@ extern "C" {
 /**
  * @brief 指定函数或变量的段位置, 如 data 或 bss.
  */
-#define __section(x)                    __attribute__((section(x)))
+#   define __section(x)                 __attribute__((section(x)))
 #endif
 
 #if !defined(__always_inline)
 /**
  * @brief 强制内联.
  */
-#define __always_inline                 inline __attribute__((__always_inline__))
+#   define __always_inline              inline __attribute__((__always_inline__))
 #endif
 
 #if !defined(__noinline)
 /**
  * @brief 不要内联.
  */
-#define __noinline                      __attribute__((__noinline__))
+#   define __noinline                   __attribute__((__noinline__))
 #endif
 
 #if !defined(__optimize)
@@ -118,14 +118,14 @@ extern "C" {
  *
  * @param level 优化等级.
  */
-#define __optimize(level)               __attribute__((__optimize__(level)))
+#   define __optimize(level)            __attribute__((__optimize__(level)))
 #endif
 
 #if !defined(__alias)
 /**
  * @brief 指定新名称.
  */
-#define __alias(x)                      __attribute__((alias(x)))
+#   define __alias(x)                   __attribute__((alias(x)))
 #endif
 
 #if !defined(__compiletime_warning)
@@ -134,7 +134,7 @@ extern "C" {
  *
  * @param message 警告消息.
  */
-#define __compiletime_warning(message)  __attribute__((warning(message)))
+#   define __compiletime_warning(message)   __attribute__((warning(message)))
 #endif
 
 #if !defined(__compiletime_error)
@@ -143,7 +143,7 @@ extern "C" {
  *
  * @param message 报错消息.
  */
-#define __compiletime_error(message)    __attribute__((error(message)))
+#   define __compiletime_error(message)     __attribute__((error(message)))
 #endif
 
 #if XF_COM_USE_BUILTIN
@@ -240,61 +240,61 @@ void xf_write_once_size(volatile void *p, void *res, int size)
     })
 
 #if !defined(__fallthrough)
-#define __fallthrough
+#   define __fallthrough
 #endif
 
 #else /* !XF_COM_USE_GNU */
 
 #if !defined(__weak)
-#define __weak
+#   define __weak
 #endif
 
 #if !defined(__used)
-#define __used
+#   define __used
 #endif
 
 #if !defined(__const)
-#define __const
+#   define __const
 #endif
 
 #if !defined(__deprecated)
-#define __deprecated
+#   define __deprecated
 #endif
 
 #if !defined(__aligned)
-#define __aligned(x)
+#   define __aligned(x)
 #endif
 
 #if !defined(__packed)
-#define __packed
+#   define __packed
 #endif
 
 #if !defined(__section)
-#define __section(x)
+#   define __section(x)
 #endif
 
 #if !defined(__always_inline)
-#define __always_inline
+#   define __always_inline
 #endif
 
 #if !defined(__noinline)
-#define __noinline
+#   define __noinline
 #endif
 
 #if !defined(__optimize)
-#define __optimize(level)
+#   define __optimize(level)
 #endif
 
 #if !defined(__alias)
-#define __alias(x)
+#   define __alias(x)
 #endif
 
 #if !defined(__compiletime_warning)
-#define __compiletime_warning(message)
+#   define __compiletime_warning(message)
 #endif
 
 #if !defined(__compiletime_error)
-#define __compiletime_error(message)
+#   define __compiletime_error(message)
 #endif
 
 #if !defined(likely)
@@ -306,14 +306,18 @@ void xf_write_once_size(volatile void *p, void *res, int size)
 #endif
 
 #if !defined(__format_attribute)
-#define __format_attribute(fmtstr, vararg)
+#   define __format_attribute(fmtstr, vararg)
 #endif
 
 #if !defined(__fallthrough)
-#define __fallthrough
+#   define __fallthrough
 #endif
 
 #endif /* XF_COM_USE_GNU */
+
+#if !defined(XF_ATTR_FAST_MEM)
+#   define XF_ATTR_FAST_MEM
+#endif
 
 /* TODO 其他编译器情况 */
 
