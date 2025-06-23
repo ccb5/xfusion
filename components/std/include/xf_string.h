4712d3e9fe269e0890e1380a0c235a52831b82f2
ccb5 <b94e@outlook.com>
2025-06-23 11:57:08 +0800

    🦄 refactor(std): 移除内联

--- components/std/include/xf_string.h
+++ components/std/include/xf_string.h
@@ -30,23 +30,19 @@ void *xf_memcpy(void *dst, const void *src, size_t len);
 void xf_memset(void *dst, uint8_t v, size_t len);
 void *xf_memmove(void *dst, const void *src, size_t len);
 int xf_memcmp(const void *p1, const void *p2, size_t len);
-static inline void xf_memzero(void *dst, size_t len);
 size_t xf_strlen(const char *str);
 size_t xf_strnlen(const char *str, size_t maxlen);
 size_t xf_strlcpy(char *dst, const char *src, size_t dst_size);
 char *xf_strncpy(char *dst, const char *src, size_t dest_size);
 char *xf_strcpy(char *dst, const char *src);
 int xf_strcmp(const char *s1, const char *s2);
-char *xf_strdup(const char *src);
+// char *xf_strdup(const char *src);
 char *xf_strcat(char *dst, const char *src);
 char *xf_strncat(char *dst, const char *src, size_t src_len);
 
-static inline void xf_memzero(void *dst, size_t len)
-{
-    xf_memset(dst, 0x00, len);
-}
-
 /* ==================== [Macros] ============================================ */
+
+#define xf_memzero(dst, len) xf_memset((dst), 0x00, (len))
 
 #ifdef __cplusplus
 } /* extern "C" */
