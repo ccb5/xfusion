dd8fe8a8a4ccc38abdd3b5f6d1b764d82de7b2ea
ccb5 <b94e@outlook.com>
2025-06-24 16:37:46 +0800

    🦄 refactor(xf_am): 消除警告

--- components/algo/xf_arithmetic.c
+++ components/algo/xf_arithmetic.c
@@ -117,20 +117,20 @@ uint32_t xf_am_swap_u32(uint32_t n)
 #if XF_COM_USE_GNU /* (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5) */
     return __builtin_bswap32(n);
 #else
-    n = (n & 0xF0F0F0F0) >> 4 | (n & 0x0F0F0F0F) << 4;
-    n = (n & 0xCCCCCCCC) >> 2 | (n & 0x33333333) << 2;
-    n = (n & 0xAAAAAAAA) >> 1 | (n & 0x55555555) << 1;
+    n = ((n & 0xF0F0F0F0) >> 4) | ((n & 0x0F0F0F0F) << 4);
+    n = ((n & 0xCCCCCCCC) >> 2) | ((n & 0x33333333) << 2);
+    n = ((n & 0xAAAAAAAA) >> 1) | ((n & 0x55555555) << 1);
     return n;
 #endif
 }
 
 uint32_t xf_am_reverse_u32(uint32_t n)
 {
-    n = (n & 0x55555555) <<  1 | (n & 0xAAAAAAAA) >>  1;
-    n = (n & 0x33333333) <<  2 | (n & 0xCCCCCCCC) >>  2;
-    n = (n & 0x0F0F0F0F) <<  4 | (n & 0xF0F0F0F0) >>  4;
-    n = (n & 0x00FF00FF) <<  8 | (n & 0xFF00FF00) >>  8;
-    n = (n & 0x0000FFFF) << 16 | (n & 0xFFFF0000) >> 16;
+    n = ((n & 0x55555555) <<  1) | ((n & 0xAAAAAAAA) >>  1);
+    n = ((n & 0x33333333) <<  2) | ((n & 0xCCCCCCCC) >>  2);
+    n = ((n & 0x0F0F0F0F) <<  4) | ((n & 0xF0F0F0F0) >>  4);
+    n = ((n & 0x00FF00FF) <<  8) | ((n & 0xFF00FF00) >>  8);
+    n = ((n & 0x0000FFFF) << 16) | ((n & 0xFFFF0000) >> 16);
     return n;
 }
 
