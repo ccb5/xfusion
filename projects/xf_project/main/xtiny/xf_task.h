32b9335ec3fe1a4807e97cfa8f2e92732f8b74c3
ccb5 <b94e@outlook.com>
2025-06-24 11:08:09 +0800

    🦄 refactor(xf_task): 更新参数名

--- projects/xf_project/main/xtiny/xf_task.h
+++ projects/xf_project/main/xtiny/xf_task.h
@@ -46,6 +46,7 @@ xf_err_t xf_task_sched_deinit(void);
 /**
  * @brief 任务句柄转任务 ID.
  *
+ * @param task          任务句柄。
  * @return xf_task_id_t
  *      - XF_TASK_ID_INVALID    无效句柄
  *      - OTHER                 任务 ID
@@ -55,6 +56,7 @@ xf_task_id_t xf_task_to_id(const xf_task_t *task);
 /**
  * @brief 任务 ID 转任务句柄.
  *
+ * @param id            任务 ID。
  * @return xf_task_t *
  *      - NULL                  无效 ID
  *      - OTHER                 任务句柄
@@ -91,45 +93,46 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
  *
  * @note 通常不需要手动销毁，在 @ref xf_task_end 分支结束后会自动销毁。
  *
- * @param _task         需要销毁的任务。 @ref xf_task_t.
+ * @param _p_task       需要销毁的任务。 @ref xf_task_t* .
  * @param _cb_func      任务函数。 @ref xf_task_cb_t.
  * @param _user_data    任务内的用户数据。 @ref xf_task_t.user_data.
  * @return xf_err_t
  *      - XF_ERR_INVALID_ARG    无效参数
  *      - XF_OK                 成功
  */
-#define xf_task_destroy(_task)          xf_task_destroy_i(_task)
+#define xf_task_destroy(_p_task)          xf_task_destroy_i(_p_task)
 
 /**
  * @brief 开始任务.
  *
  * @note 开始任务 = 创建任务 + 立即运行任务.
  *
- * @param[out] _task    传出的任务句柄。同时内部作为临时变量使用。 @ref xf_task_t 指针.
  * @param _cb_func      任务函数。 @ref xf_task_cb_t.
  * @param _user_data    任务内的用户数据。 @ref xf_task_t.user_data.
  * @param _arg          传给任务的参数。
+ * @param[out] _pp_task 传出的任务句柄。 @ref xf_task_t** .
+ *                      同时作为临时变量使用，因此必须传入。
  * @return xf_task_async_t 任务状态。
  *      - XF_TASK_READY        就绪
  *      - XF_TASK_BLOCKED      阻塞
  *      - XF_TASK_TERMINATED   结束
  */
-#define xf_task_start(_task, _cb_func, _user_data, _arg) \
-                                        xf_task_start_i((_task), (_cb_func), (_user_data), (_arg))
+#define xf_task_start(_cb_func, _user_data, _arg, _pp_task) \
+                                        xf_task_start_i((_cb_func), (_user_data), (_arg), (_pp_task))
 
 /**
  * @brief 运行任务.
  *
  * @note 运行任务 = 设置任务为就绪状态 + 调用任务.
  *
- * @param _task         要运行的任务。 @ref xf_task_t.
+ * @param _p_task       要运行的任务。 @ref xf_task_t* .
  * @param _arg          传给任务的参数。
  * @return xf_task_async_t 任务状态。
  *      - XF_TASK_READY        就绪
  *      - XF_TASK_BLOCKED      阻塞
  *      - XF_TASK_TERMINATED   结束
  */
-#define xf_task_run(_task, _arg)        xf_task_run_i((_task), (_arg))
+#define xf_task_run(_p_task, _arg)        xf_task_run_i((_p_task), (_arg))
 
 /**
  * @brief 创建子任务.
@@ -137,7 +140,7 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
  * @note 1. 父任务与子任务之间会相互记录 ID.
  * @note 2. 任务创建后不会立即执行。
  *
- * @param _me           父任务。 @ref xf_task_t.
+ * @param _me           父任务。 @ref xf_task_t* .
  * @param _cb_func      任务函数。 @ref xf_task_cb_t.
  * @param _user_data    任务内的用户数据。 @ref xf_task_t.user_data.
  * @return xf_task_t*   创建的任务
@@ -153,7 +156,7 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
  * @note 仅当子任务执行完毕后才会继续执行当前任务后面的代码。
  * @warning
  *
- * @param _me           当前任务。 @ref xf_task_t.
+ * @param _me           当前任务。 @ref xf_task_t* .
  * @param _arg          传给子任务的参数。
  */
 #define xf_task_wait_subtask(_me, _arg) \
@@ -164,7 +167,7 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
  *
  * @note 等待子任务 = 创建子任务 + 等待子任务结束.
  *
- * @param _task         需要销毁的任务。 @ref xf_task_t.
+ * @param _p_task       需要销毁的任务。 @ref xf_task_t* .
  * @param _cb_func      任务函数。 @ref xf_task_cb_t.
  * @param _user_data    任务内的用户数据。 @ref xf_task_t.user_data.
  * @param _arg          传给任务的参数。
@@ -177,7 +180,7 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
  *
  * @note 任务块起始 = 设置系统变量 + 恢复 lc.
  *
- * @param _me           当前任务。 @ref xf_task_t.
+ * @param _me           当前任务。 @ref xf_task_t* .
  */
 #define xf_task_begin(_me)              xf_task_begin_i(_me)
 
@@ -186,38 +189,38 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
  *
  * @note 任务块结束 = 重置 lc + 销毁任务 (含 @ref xf_task_t.user_data).
  *
- * @param _me           当前任务。 @ref xf_task_t.
+ * @param _me           当前任务。 @ref xf_task_t* .
  */
 #define xf_task_end(_me)                xf_task_end_i(_me)
 
 /**
  * @brief 获取任务状态.
  *
- * @param _task         任务句柄。 @ref xf_task_t.
+ * @param _p_task       任务句柄。 @ref xf_task_t* .
  * @return xf_task_async_t @ref xf_task_async_t
  *      - XF_TASK_READY        就绪
  *      - XF_TASK_BLOCKED      阻塞
  *      - XF_TASK_TERMINATED   结束
  */
-#define xf_task_get_state(_task)        xf_task_attr_get_state(_task)
+#define xf_task_get_state(_p_task)      xf_task_attr_get_state(_p_task)
 
 /**
  * @brief 设置任务为就绪状态.
  *
  * @note 通常不需要设置，任务创建后会自动设置为就绪状态。
  *
- * @param _task         任务句柄。 @ref xf_task_t.
+ * @param _p_task       任务句柄。 @ref xf_task_t* .
  */
-#define xf_task_set_ready(_task)        xf_task_attr_set_state((_task), XF_TASK_READY)
+#define xf_task_set_ready(_p_task)      xf_task_attr_set_state((_p_task), XF_TASK_READY)
 
 /**
  * @brief 设置任务为阻塞状态.
  *
  * @note 通常不需要设置。
  *
- * @param _task         任务句柄。 @ref xf_task_t.
+ * @param _p_task       任务句柄。 @ref xf_task_t* .
  */
-#define xf_task_set_blocked(_task)      xf_task_attr_set_state((_task), XF_TASK_BLOCKED)
+#define xf_task_set_blocked(_p_task)    xf_task_attr_set_state((_p_task), XF_TASK_BLOCKED)
 
 /**
  * @brief 重启当前任务.
@@ -225,7 +228,7 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
  * @note 不会立刻回到 beging 重新执行，
  *       需要下一次进入后才能重新执行（为了 begin 前的初始化正常工作）。
  *
- * @param _me           当前任务。 @ref xf_task_t.
+ * @param _me           当前任务。 @ref xf_task_t* .
  */
 #define xf_task_restart(_me)            xf_task_restart_i(_me)
 
@@ -234,7 +237,7 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
  *
  * @note 让出一次，让出后任务会进入就绪状态，等待下一次 xf_task_sched() 调度。
  *
- * @param _me           当前任务。 @ref xf_task_t.
+ * @param _me           当前任务。 @ref xf_task_t* .
  */
 #define xf_task_yield(_me)              xf_task_yield_i(_me)
 
@@ -243,14 +246,14 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
  *
  * @warning 如果没有唤醒源，任务会一直阻塞，请勿随意调用。
  *
- * @param _me           当前任务。 @ref xf_task_t.
+ * @param _me           当前任务。 @ref xf_task_t* .
  */
 #define xf_task_block(_me)              xf_task_block_i(_me)
 
 /**
  * @brief 任务延时.
  *
- * @param _me           当前任务。 @ref xf_task_t.
+ * @param _me           当前任务。 @ref xf_task_t* .
  * @param _tick         延时时间，单位 (tick)。 @ref xf_tick_t.
  */
 #define xf_task_delay(_me, _tick)       xf_task_delay_i((_me), (_tick))
@@ -258,7 +261,7 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
 /**
  * @brief 任务 ms 延时.
  *
- * @param _me           当前任务。 @ref xf_task_t.
+ * @param _me           当前任务。 @ref xf_task_t* .
  * @param _ms           延时时间，单位 (ms)。 @ref xf_tick_t.
  */
 #define xf_task_delay_ms(_me, _ms)      xf_task_delay_ms_i((_me), (_ms))
@@ -266,28 +269,30 @@ xf_task_t *xf_task_id_to_task(xf_task_id_t id);
 /**
  * @brief 任务等待事件.
  *
- * @param _me           当前任务。 @ref xf_task_t.
+ * @param _me           当前任务。 @ref xf_task_t* .
  * @param _id           事件 ID。 @ref xf_event_id_t.
  * @param _tick         等待时间，单位 (tick)。 @ref xf_tick_t.
- * @param[out] _xf_err  错误码。 @ref xf_err_t.
- *                      - XF_ERR_TIMEOUT     超时
- *                      - XF_OK              成功
+ * @param[out] _p_xf_err    传出错误码。填入 NULL 时不传出。 @ref xf_err_t* .
+ *                          - XF_ERR_TIMEOUT     超时
+ *                          - XF_OK              成功
+ * @param[out] _p_e_msg     传出事件消息。填入 NULL 时不传出。 @ref xf_event_msg_t* .
  */
-#define xf_task_wait_until(_me, _id, _tick, _xf_err) \
-                                        xf_task_wait_until_i((_me), (_id), (_tick), (_xf_err))
+#define xf_task_wait_until(_me, _id, _tick, _p_xf_err, _p_e_msg) \
+                                        xf_task_wait_until_i((_me), (_id), (_tick), (_p_xf_err), (_p_e_msg))
 
 /**
  * @brief 任务等待事件，单位 (ms).
  *
- * @param _me           当前任务。 @ref xf_task_t.
+ * @param _me           当前任务。 @ref xf_task_t* .
  * @param _id           事件 ID。 @ref xf_event_id_t.
  * @param _ms           等待时间，单位 (ms)。 @ref xf_tick_t.
- * @param[out] _xf_err  错误码。 @ref xf_err_t.
- *                      - XF_ERR_TIMEOUT     超时
- *                      - XF_OK              成功
+ * @param[out] _p_xf_err    传出错误码。填入 NULL 时不传出。 @ref xf_err_t* .
+ *                          - XF_ERR_TIMEOUT     超时
+ *                          - XF_OK              成功
+ * @param[out] _p_e_msg     传出事件消息。填入 NULL 时不传出。 @ref xf_event_msg_t* .
  */
-#define xf_task_wait_until_ms(_me, _id, _ms, _xf_err) \
-                                        xf_task_wait_until_ms_i((_me), (_id), (_ms), (_xf_err))
+#define xf_task_wait_until_ms(_me, _id, _ms, _p_xf_err, _p_e_msg) \
+                                        xf_task_wait_until_ms_i((_me), (_id), (_ms), (_p_xf_err), (_p_e_msg))
 
 #ifdef __cplusplus
 } /* extern "C" */
